f06c4063184c050b44bf57da45fa3282532578ea
Hatam-Jafarov <hatam.jafarov@gmail.com>
2020-08-24 22:37:43 +0400

    search view

--- src/js/index.js
+++ src/js/index.js
@@ -5,25 +5,28 @@
 //https://api.themoviedb.org/3/search/movie?api_key=41728cc89ddb95370acfded3f1e2f7ab&language=en-US&query=abc&page=1&include_adult=false
 
 import Search from './models/Search';
+import {elements} from './base';
+import * as searchView from './views/searchView';
 
 const state = {};
 
 const searchController = async () => {
-    const keyword = document.getElementById('txt-keyword').value;
+    const keyword = elements.searchInput.value;
+    console.log(keyword)
     if(keyword){
         state.search = new Search(keyword);
 
         await state.search.getResults();
-        console.log(state.search.data)
+        searchView.displayResults(state.search.data)
     }
     else{
         alert('enter keyword')
     }
 }
 
-
-document.getElementById('form-search').addEventListener('submit',function(e){
+elements.searchForm.addEventListener('submit',function(e){
     e.preventDefault();
     searchController();
     console.log('form-submitted');
+    console.log(elements)
 })
