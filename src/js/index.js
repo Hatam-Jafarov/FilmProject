6429b6bf6dc7f2fc8bec65bab3ffad759aac60a0
Hatam-Jafarov <hatam.jafarov@gmail.com>
2020-08-24 21:49:19 +0400

    search controller

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,26 @@
 //request: 
 //https://api.themoviedb.org/3/search/movie?api_key=41728cc89ddb95370acfded3f1e2f7ab&language=en-US&query=abc&page=1&include_adult=false
 
-
-
 import Search from './models/Search';
-const search = new Search('abc');
 
-console.log(search);
-search.getResults();
+const state = {};
+
+const searchController = async () => {
+    const keyword = document.getElementById('txt-keyword').value;
+    if(keyword){
+        state.search = new Search(keyword);
+
+        await state.search.getResults();
+        console.log(state.search.data)
+    }
+    else{
+        alert('enter keyword')
+    }
+}
+
+
+document.getElementById('form-search').addEventListener('submit',function(e){
+    e.preventDefault();
+    searchController();
+    console.log('form-submitted');
+})
